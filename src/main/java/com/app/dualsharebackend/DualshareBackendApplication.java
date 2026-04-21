5e88353dc8daa7668794150774b1b0e629f8a8c5
Emir Guanactolay <emir201xd@gmail.com>
2026-04-21 19:59:41 -0300

    inicio

---
+++ src/main/java/com/app/dualsharebackend/DualshareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.app.dualsharebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DualshareBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DualshareBackendApplication.class, args);
+    }
+
+}
